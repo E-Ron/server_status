508a06af256259af7a1c4130a9ed8285bdf30bd5
LimitedAsm <yurev74sn@mail.ru>
2023-04-11 22:08:01 +0300

    fix online status undefine not processed

--- lib/modules/server_status/screens/server_status_screen.dart
+++ lib/modules/server_status/screens/server_status_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:minecraft_server_status/modules/server_status/models/server_status.dart';
 
 import 'package:minecraft_server_status/widgets/loading_animation.dart';
 import '../bloc/server_status_bloc.dart';
@@ -13,19 +14,19 @@ class ServerStatusScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ServerStatusBloc, ServerStatusState>(
         builder: (context, state) {
-      switch (state.fetchStatus) {
-        case ServerFetchStatus.loading:
-          return const LoadingAnimation();
-        case ServerFetchStatus.failure:
-          return Center(
-            child: Text(
-              AppLocalizations.of(context)!.playersListError,
-              textAlign: TextAlign.center,
-              style: const TextStyle(height: 2),
-            ),
-          );
-        case ServerFetchStatus.success:
-          return PlayersList(players: state.serverStatus.players);
+      if (state.fetchStatus == ServerFetchStatus.loading) {
+        return const LoadingAnimation();
+      } else if (state.fetchStatus == ServerFetchStatus.failure ||
+          state.serverStatus.onlineStatus == OnlineStatus.undefine) {
+        return Center(
+          child: Text(
+            AppLocalizations.of(context)!.playersListError,
+            textAlign: TextAlign.center,
+            style: const TextStyle(height: 2),
+          ),
+        );
+      } else {
+        return PlayersList(players: state.serverStatus.players);
       }
     });
   }
